e3c5810b466b1c05d9cbec67eff3b24eed123995
Jacob Vartuli <jacob.vartuli.92@gmail.com>
2018-07-16 10:10:01 -0400

    Done.

--- queries.py
+++ queries.py
@@ -7,34 +7,24 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 def return_teams_for_new_york():
-    # here we want to return all teams that are associated with New York City
-    pass
-
+    return session.query(City).filter_by(name = 'New York')[0].team
 def return_players_for_la_dodgers():
-    # here we want to return all players that are associated with the LA dodgers
-    pass
-
+    return session.query(Team).filter_by(name = 'L.A. Dodgers')[0].players
 def return_sorted_new_york_knicks():
-    # here we want to return all the players on the New York Knicks
-    # sorted in ascending (small -> big) order by their number
-    pass
-
+    return session.query(Team).filter_by(name = 'NY Knicks')[0].players
 def return_youngest_basket_ball_player_in_new_york():
+    return [obj[0] for obj in session.query(Player, Team).filter(Team.id == Player.team_id).filter(Team.name == 'NY Knicks').order_by(Player.age).all()][0]
     # here we want to sort all the players on New York Knicks by age
     # and return the youngest player
-    pass
-
 def return_all_players_in_los_angeles():
+    return [_tuple[0].players for _tuple in session.query(Team,City).filter((Team.city_id == City.id) & (City.name == 'Los Angeles')).all()]
     # here we want to return all players that are associated with
     # a sports team in LA
-    pass
-
 def return_tallest_player_in_los_angeles():
+    return session.query(Player).order_by(Player.height.desc()).all()[0]
     # here we want to return the tallest player associted with
     # a sports team in LA
-    pass
-
 def return_team_with_heaviest_players():
+    return session.query(Player).order_by(Player.weight.desc()).all()[0].team
     # here we want to return the city with the players that
     # have the heaviest average weight (total weight / total players)
-    pass
